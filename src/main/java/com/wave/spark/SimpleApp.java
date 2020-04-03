266c4f535c4db42a23181686191d8e8227eb66e3
amit <amitwave@gmail.com>
2020-04-03 13:30:11 +0100

    first commit

---
+++ src/main/java/com/wave/spark/SimpleApp.java
@@ -0,0 +1,22 @@
+package com.wave.spark;
+
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+
+        System.out.println("in the spark   ");
+        String logFile = "/usr/local/spark/README.md"; // Should be some file on your system
+        SparkSession spark = SparkSession.builder().appName("Simple Application").config("spark.master", "local").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
+    }
+}
